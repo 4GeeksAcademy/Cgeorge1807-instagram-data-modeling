0a2cb6b1692ebff3e228ff34be1db10b98fbc650
Cgeorge1807 <christian03george@gmail.com>
2025-02-18 00:47:40 +0000

    en desarrollo

--- src/models.py
+++ src/models.py
@@ -7,32 +7,25 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-
-
-class Address(Base):
-    __tablename__ = 'address'
+class User (Base):
+    __tablename__ = 'user'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    firstname = Column(String(250),nullable=False)
+    lastname = Column(String(250),nullable=False)
+    username = Column(String(250),nullable=False)
 
-    def to_dict(self):
-        return {}
 
-## Draw from SQLAlchemy base
-try:
-    result = render_er(Base, 'diagram.png')
-    print("Success! Check the diagram.png file")
-except Exception as e:
-    print("There was a problem genering the diagram")
-    raise e
+class Post(Base):
+    __tablename__ = 'post'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    url_image = Column(String(250),nullable=False)
+    likes = Column(Integer)
+    description = Column(String(250))
+    person_id = Column(Integer, ForeignKey('user.id'))
+    person = relationship(User)
+
+       
