ed33f51aa16606ce04bc50604f80b6214b604a54
Cgeorge1807 <christian03george@gmail.com>
2025-02-19 16:43:53 +0000

    Finalizado

--- src/models.py
+++ src/models.py
@@ -25,7 +25,34 @@ class Post(Base):
     url_image = Column(String(250),nullable=False)
     likes = Column(Integer)
     description = Column(String(250))
-    person_id = Column(Integer, ForeignKey('user.id'))
-    person = relationship(User)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
-       
+class Comment (Base):
+    __tablename__ = 'comment'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer)
+    text = Column(String(250))
+    post_id = Column(Integer, ForeignKey('post.id')) 
+    post = relationship(Post)
+
+class Follower (Base):
+    __tablename__ = 'follower'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_from_id = Column(Integer,ForeignKey('post.id'))
+    user = relationship(Post)
+    user_to_id = Column(Integer,ForeignKey('Post.id'))
+    user = relationship (Post)
+
+
+## Draw from SQLAlchemy base
+try:
+    result = render_er(Base, 'diagram.png')
+    print("Success! Check the diagram.png file")
+except Exception as e:
+    print("There was a problem genering the diagram")
+    raise e    
